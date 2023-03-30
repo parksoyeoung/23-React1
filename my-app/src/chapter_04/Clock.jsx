9c62c024ef107959706351f3cd422183ab317981
parksoyeoung <qkrthdud2325@email.daelim.ac.kr>
2023-03-30 12:50:37 +0900

    5주차

---
+++ my-app/src/chapter_04/Clock.jsx
@@ -0,0 +1,15 @@
+//시계만들기 p.132
+//clock 컴포넌트
+
+import React from "react";
+
+function Clock(props){
+  return (
+    <div>
+      <h1>안녕 리액트</h1>
+      <h2> 현재 시간: {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+export default Clock;
